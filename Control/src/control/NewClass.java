6d4a66b175abe1bedce921190f270b1c26806eda
Sandra Cañas <Sandra Caas@DESKTOP-DUAQ3FH>
2019-02-09 11:36:10 -0500

    agregué NewClass.java

---
+++ Control/src/control/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author Sandra Cañas
+ */
+public class NewClass {
+    public int suma (int a, int b){
+        int sum =a+b;
+        return (sum);
+    }
+}
