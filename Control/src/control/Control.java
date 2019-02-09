5cb0a975dda7dc8377396eb94f80ab5ddaae82d8
Sandra Cañas <Sandra Caas@DESKTOP-DUAQ3FH>
2019-02-09 09:19:59 -0500

    agregue Hola luis 2

--- Control/src/control/Control.java
+++ Control/src/control/Control.java
@@ -5,7 +5,7 @@ public class Control {
     public static void main(String[] args) {     
         System.out.println("Version 1");
         System.out.println("Version 2");
-        System.out.println("Hola  Luis ");
+        System.out.println("Hola  Luis 2");
     }
     
 }
