b0461b646015791638a2d1680d894b0c43c03949
luis <luis@DESKTOP-1QJSHH1>
2019-02-09 09:20:19 -0500

    Cambio linea 3

--- Control/src/control/Control.java
+++ Control/src/control/Control.java
@@ -5,7 +5,7 @@ public class Control {
     public static void main(String[] args) {     
         System.out.println("Version 1");
         System.out.println("Version 2");
-        System.out.println("Hola  Luis 2");
+        System.out.println("Luis Hola :v");
     }
     
 }
