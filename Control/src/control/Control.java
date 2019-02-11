e8c728166217c54a72bc1922b968f9e8516dc423
Sandra Cañas <Sandra Caas@DESKTOP-DUAQ3FH>
2019-02-11 09:54:09 -0500

    se agrego 11 de feb by sami

--- Control/src/control/Control.java
+++ Control/src/control/Control.java
@@ -5,7 +5,8 @@ public class Control {
     public static void main(String[] args) {     
         System.out.println("Version 1");
         System.out.println("Version 2");
-        System.out.println("Parangaricuatirimicuaro");       
+        System.out.println("Parangaricuatirimicuaro");  
+        System.out.println("11 de febrero by Sami");
     }
     
 }
